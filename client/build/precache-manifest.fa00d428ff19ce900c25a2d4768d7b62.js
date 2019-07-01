ca915488886dda7ba409f0ecc6dbf42e4aa683d2
Qbisst <ismaelaperez@outlook.com>
2019-06-30 22:27:02 -0400

    preparing for heroku deployment

---
+++ client/build/precache-manifest.fa00d428ff19ce900c25a2d4768d7b62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7186ee809f3b834d2148dbceccef39a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc08c4f08c25979bdc1",
+    "url": "/static/js/2.d10b0f11.chunk.js"
+  },
+  {
+    "revision": "683d1faeaabd93497ec4",
+    "url": "/static/js/main.ae722ce0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
