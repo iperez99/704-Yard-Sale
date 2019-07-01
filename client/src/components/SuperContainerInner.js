96d85593ee1f3d8db29ba552720bbfcd989e3bdc
Qbisst <ismaelaperez@outlook.com>
2019-07-01 17:37:00 -0400

    removed comment in SuperContainerInner.js

--- client/src/components/SuperContainerInner.js
+++ client/src/components/SuperContainerInner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SuperContainerInner(props) {
     return (
         <div className="super_container_inner">
-            {/* <div class="super_overlay"></div> */}
+
             {props.children}
         </div>
     );
